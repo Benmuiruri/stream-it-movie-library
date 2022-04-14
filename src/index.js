2d50f81c411d83a8d6b90c2b019d8c1539d4bcb1
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-14 15:45:44 +0300

    Add movie list counter to homepage

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import movieModal from './modules/movie-modal.js';
 
 window.addEventListener('load', async () => {
   const movies = await getMovies();
-  const sampleMovies = movies.slice(0, 48);
+  const sampleMovies = movies.slice(0, 36);
   await displayMovies(sampleMovies);
+
+  const movieList = document.querySelector('.movie-list');
+  movieList.innerHTML = `We currently have <span> ${sampleMovies.length} <span> movies`;
 
   const commentButtons = [...document.querySelectorAll('.commentBtn')];
   movieModal(commentButtons, sampleMovies);
