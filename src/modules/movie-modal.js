7ad0cfd360566d164a6ac0fb3da059988ca52e4b
Olawale Bamidele <bolawale81@gmail.com>
2022-04-14 05:30:54 +0100

    Comment button to popup more details

--- src/modules/movie-modal.js
+++ src/modules/movie-modal.js
@@ -1,9 +1,105 @@
-const movieModal = async (commentButtons, sampleMovies) => { //eslint-disable-line
-  commentButtons.forEach((button, i) => { //eslint-disable-line
+const isVisible = 'is-visible';
+
+const movieModal = async (commentButtons, sampleMovies) => {
+  commentButtons.forEach((button, i) => {
     button.addEventListener('click', async () => {
-      alert('I have been clicked');
+      // Create movie modal
+      const modal = document.createElement('article');
+      modal.className = 'modal';
+      // Create div inside modal
+      const popUpDiv = document.createElement('div');
+      popUpDiv.className = 'modal-dialog';
+      modal.classList.add(isVisible);
+      modal.appendChild(popUpDiv);
+      // Create image with buttons inside div
+      const movieImgDiv = document.createElement('div');
+      const movieImg = document.createElement('img');
+      const buttonDiv = document.createElement('div');
+      const movieDownloadBtn = document.createElement('button');
+      const movieWatchBtn = document.createElement('button');
+      movieImgDiv.className = 'movieCard';
+      movieImg.className = 'movieCard-img';
+      movieImg.src = `${sampleMovies[i].image.original}`;
+      buttonDiv.className = 'buttonDiv';
+      movieDownloadBtn.className = 'popUp-button';
+      movieDownloadBtn.innerHTML = '<i class="fa-solid fa-angles-down"></i>Download';
+      movieWatchBtn.className = 'popUp-button';
+      movieWatchBtn.innerHTML = 'Watch Now';
+      movieImgDiv.appendChild(movieImg);
+      buttonDiv.appendChild(movieDownloadBtn);
+      buttonDiv.appendChild(movieWatchBtn);
+      movieImgDiv.appendChild(buttonDiv);
+      popUpDiv.appendChild(movieImgDiv);
+
+      // Create Movie Details inside div
+      const movieContentDiv = document.createElement('div');
+      const movieTitle = document.createElement('h1');
+      const movieGenre = document.createElement('h2');
+      const movieReleaseDate = document.createElement('h2');
+      const movieNetwork = document.createElement('h1');
+      const downloadSubtitle = document.createElement('a');
+      const ratingStar = document.createElement('i');
+      const movieRating = document.createElement('span');
+      const movieSummary = document.createElement('p');
+      movieContentDiv.className = 'movieContent';
+      movieTitle.textContent = `${sampleMovies[i].name}`;
+      movieTitle.className = 'popUp-title';
+      movieGenre.innerHTML = `${sampleMovies[i].genres[0]}`;
+      movieGenre.className = 'popUp-Genre';
+      movieReleaseDate.innerHTML = `${sampleMovies[i].premiered}`;
+      movieReleaseDate.className = 'popUp-releaseDate';
+      movieNetwork.innHTML = '';
+      movieNetwork.className = '';
+      downloadSubtitle.href = '#';
+      downloadSubtitle.innerHTML = '<i class="fa-solid fa-angles-down"></i> Download Subtitles';
+      downloadSubtitle.className = 'subtitle-button';
+      ratingStar.classList.add('fa-solid');
+      ratingStar.classList.add('fa-star');
+      ratingStar.classList.add('overlay-star');
+      movieRating.className = 'movie-overlay-rating';
+      if (sampleMovies[i].rating.average) {
+        movieRating.innerHTML = `${sampleMovies[i].rating.average} / 10`;
+      } else {
+        movieRating.innerHTML = '0 / 10';
+      }
+      movieSummary.innerHTML = `${sampleMovies[i].summary.substring(0, 200)}`;
+      movieSummary.className = 'popUp-summary';
+      movieContentDiv.appendChild(movieTitle);
+      movieContentDiv.appendChild(movieGenre);
+      movieContentDiv.appendChild(movieReleaseDate);
+      movieContentDiv.appendChild(downloadSubtitle);
+      movieContentDiv.appendChild(ratingStar);
+      movieContentDiv.appendChild(movieRating);
+      movieContentDiv.appendChild(movieSummary);
+      popUpDiv.appendChild(movieContentDiv);
+
+      // Close button
+      const closeModalBtn = document.createElement('button');
+      closeModalBtn.innerHTML = '';
+      closeModalBtn.className = 'close-proj-modal';
+      popUpDiv.appendChild(closeModalBtn);
+      closeModalBtn.addEventListener('click', () => {
+        modal.classList.remove('is-visible');
+      });
+
+      // Read more button
+      const readMore = document.createElement('button');
+      readMore.innerHTML = 'Read More';
+      readMore.className = 'readMoreBtn';
+      movieContentDiv.appendChild(readMore);
+      readMore.addEventListener('click', () => {
+        if (readMore.innerHTML === 'Read More') {
+          readMore.innerHTML = 'Read Less';
+          movieSummary.innerHTML = `${sampleMovies[i].summary}`;
+        } else {
+          readMore.innerHTML = 'Read More';
+          movieSummary.innerHTML = `${sampleMovies[i].summary.substring(0, 400)}`;
+        }
+      });
+
+      // Add modal to body
+      document.body.appendChild(modal);
     });
   });
 };
-
 export default movieModal;
