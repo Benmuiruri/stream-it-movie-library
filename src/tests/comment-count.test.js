7f085831c3c9fd69db16ae3c4019d62d56826f58
Benmuiruri <ben.muiruri13@gmail.com>
2022-04-14 17:51:10 +0300

    Add comments counter test

--- src/tests/comment-count.test.js
+++ src/tests/comment-count.test.js
@@ -3,8 +3,14 @@ import { countComments } from '../modules/comments-handler';
 describe('Display number of comments', () => {
   test('should display 0 Comments', () => {
     const commentDiv = { textContent: '' };
+    const comments = [];
+    countComments(commentDiv, comments);
+    expect(commentDiv.textContent).toBe('O Comments');
+  });
+  test('should display 1 Comments', () => {
+    const commentDiv = { textContent: '' };
     const comments = [{ data: 1 }];
     countComments(commentDiv, comments);
-    expect(commentDiv.textContent).toBe('0 Comments');
+    expect(commentDiv.textContent).toBe('1 Comments');
   });
 });
